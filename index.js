95abae63dfd1161b7a0fd0edf174ae784c29edc8
Ethan May <ethan@ethanmay.com>
2025-07-02 11:00:33 -0400

    Enable custom extensions, rename files (#2)
    * Enable custom extensions, rename files
    
    * Bump version number

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // eslint-disable-next-line import/extensions
 const baseConfig = require('10up-toolkit/config/postcss.config.js');
-const { additionalPlugins, preLoadPlugins, mixins } = require('./postcss/config');
+const additionalPlugins = require('./lib/plugins');
+const mixins = require('./lib/mixins');
+const functions = require('./lib/functions');
 
-module.exports = (props) => {
-	const config = baseConfig(props);
+module.exports = ({
+	customAdditionalPlugins = {},
+	customPreloadPlugins = {},
+	customMixins = {},
+	customVariables = {},
+	customFunctions = {},
+} = {}) => {
+	const config = baseConfig();
 
 	if (config.plugins['postcss-preset-env']) {
 		config.plugins['postcss-preset-env'].stage = 0;
@@ -13,13 +21,35 @@ module.exports = (props) => {
 	}
 
 	if (config.plugins['postcss-mixins']) {
-		config.plugins['postcss-mixins'].mixins = mixins;
+		config.plugins['postcss-mixins'].mixins = {
+			...mixins,
+			...customMixins,
+		};
 	}
 
 	config.plugins = {
-		...preLoadPlugins,
+		...{
+			'postcss-simple-vars': {
+				variables() {
+					// eslint-disable-next-line global-require
+					return {
+						...require('./variables'),
+						...customVariables,
+					};
+				},
+				silent: true,
+			},
+		},
+		...customPreloadPlugins,
 		...config.plugins,
 		...additionalPlugins,
+		...{
+			'postcss-functions': {
+				functions,
+				...customFunctions,
+			},
+		},
+		...customAdditionalPlugins,
 	};
 
 	return config;
